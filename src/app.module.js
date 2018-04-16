2850ff54c67a03783641a7fd18b2df97d409dc24
Julia <jkmcgeoghan@myseneca.ca>
2018-04-15 23:10:33 -0400

    Get base of site working with webpack

--- src/app.module.js
+++ src/app.module.js
@@ -51,5 +51,18 @@
 
 })();
 
-    // url: '',
-    // template: '<img-rating-page></img-rating-page>'
+require('./models/rating-page.model.js');
+require('./models/upload-page.model.js');
+require('./services/eventService.js');
+
+require('./pages/rating-page/rating-page.module.js');
+require('./pages/rating-page/rating-page.directive.js');
+require('./pages/rating-page/carousel/carousel.directive.js');
+require('./pages/rating-page/comments/comments.directive.js');
+
+require('./pages/upload-page/upload-page.module.js');
+require('./pages/upload-page/upload-page.directive.js');
+require('./pages/upload-page/uploader/uploader.directive.js');
+require('./pages/upload-page/upload-results/upload-results.directive.js');
+require('./pages/upload-page/upload-results/result/result.directive.js');
+require('./pages/upload-page/upload-form/upload-form.directive.js');
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  entry: "./src/app.module.js",
+  output: {
+    filename: "dist/bundle.js"
+  }
+}
